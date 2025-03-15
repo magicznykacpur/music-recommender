89df4f052d47e1cb46a3f557ba17edc8b4512cc7
kafejek <kacper@ultiarena.com>
2025-03-15 15:29:48 +0100

    remove class from test_env

--- src/env/test_env.py
+++ src/env/test_env.py
@@ -3,9 +3,13 @@ import os
 from setup_env import setup_env_vars
 
 
-class TestEnv:
-    def test_setup_env_vars(self):
-        setup_env_vars()
+def test_setup_env_vars():
+    setup_env_vars()
 
-        assert os.environ["SPOTIPY_CLIENT_ID"] != ""
-        assert os.environ["SPOTIPY_CLIENT_SECRET"] != ""
+    assert os.environ["SPOTIPY_CLIENT_ID"] != ""
+    assert os.environ["SPOTIPY_CLIENT_SECRET"] != ""
+
+
+def test_does_not_setup_env_vars():
+    assert os.environ["SPOTIPY_CLIENT_ID"] != ""
+    assert os.environ["SPOTIPY_CLIENT_SECRET"] != ""
